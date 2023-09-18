f8f7d842160b07f9f366142ca4844def45471df4
Banagsh0219 <assassincreed9911@gmail.com>
2023-09-19 00:21:58 +0500

    Drum-kit

--- Drum Kit Starting Files/assets/js/index.js
+++ Drum Kit Starting Files/assets/js/index.js
@@ -1,12 +1,36 @@
+const sounds = [
+	"assets/sounds/crash.mp3",
+	"assets/sounds/kick-bass.mp3",
+	"assets/sounds/snare.mp3",
+	"assets/sounds/tom-1.mp3",
+	"assets/sounds/tom-2.mp3",
+	"assets/sounds/tom-3.mp3",
+	"assets/sounds/tom-4.mp3",
+];
 
+const keys = ["w", "a", "s", "d", "j", "k", "l"];
 
-for(let i = 0; i < document.querySelectorAll('.drum').length ; i++){
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+	// handle clicks
+	document.querySelectorAll(".drum")[i].addEventListener("click", () => {
+		let sound = new Audio(sounds[i]);
+		sound.play();
+		buttonAnimation(i);
+	});
 
-    document.querySelectorAll('.drum')[i].addEventListener('click', () => {
-        alert('I am Clicked');
-    });
-
-    console.log(i);
+	// Handle key press
+	document.addEventListener("keypress", function (event) {
+		if (event.key == keys[i]) {
+			let sound = new Audio(sounds[i]);
+			sound.play();
+			buttonAnimation(i);
+		}
+	});
 }
 
-
+function buttonAnimation(counter) {
+	document.querySelectorAll(".drum")[counter].classList.add("pressed");
+	setTimeout(() => {
+		document.querySelectorAll(".drum")[counter].classList.remove("pressed");
+	}, 200);
+}
